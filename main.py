785fde57aaf49aea40f9bde94f7630b3f848619f
elik <elik@example.com>
2024-09-24 18:09:01 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, status, Query, Path, HTTPException
+import uvicorn
+
+
+users = [
+    {"id": 1, "admin": "Admin"},
+    {"id": 2, "user1": "User1"},
+    {"id": 3, "user2": "User2"}
+]
+
+app = FastAPI()
+
+
+@app.get('/users')
+async def all_users():
+    return {"users": users}
+
+
+@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
+async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
+                      title: str = Query(description="enter user")):
+    user = {"id": user_id, "title": title}
+    users.append(user)
+    return {"new user": user}
+
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int):
+    user = next((user for user in users if user["id"] == user_id), None)
+    if user:
+        users.remove(user)
+        return {"user is deleted": user}
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f" {user_id} not found")
+
+if __name__ == '__main__':
+    uvicorn.run("__main__:app", reload=True)
---
+++ test_main.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+users = [
+    {"id": 1, "admin": "Admin"},
+    {"id": 2, "user1": "User1"},
+    {"id": 3, "user2": "User2"}
+]
+
+
+def test_all_users():
+    resp = client.get('/users')
+    assert resp.status_code == 200
+    assert resp.json() == {
+        "users": users
+    }
+
+def test_user_exception():
+    resp = client.get("/users/8")
+    assert resp.status_code == 400
+    assert resp.json() == {
+        "detail": f"User with id 10 not found"
+    }
+
+def test_create_user():
+    user_id = 4
+    title = "User3"
+    resp = client.post(f"/users/{user_id}",
+                       params={"title": title}
+                       )
+    assert resp.status_code == 201
+    assert resp.json() == {
+        "new user": {"id": user_id, "title": title},
+    }
+
+
+def test_delete_user():
+    resp = client.delete(f"/users/3")
+    assert resp.status_code == 204
