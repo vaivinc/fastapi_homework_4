d31c18e897b8935cb2f81232a52d0787be803810
vaivinc <168576062+vaivinc@users.noreply.github.com>
2024-09-26 15:58:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, status, Query, Path, HTTPException
 import uvicorn
 
-
 users = [
     {"id": 1, "admin": "Admin"},
     {"id": 2, "user1": "User1"},
@@ -16,12 +15,21 @@ async def all_users():
     return {"users": users}
 
 
+@app.get('/users/{user_id}')
+async def get_user_id(user_id: int):
+    user = next((user for user in users if user["id"] == user_id), None)
+    if user:
+        return {"user": user}
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User with {user_id} not found")
+
+
 @app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
 async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
                       title: str = Query(description="enter user")):
     user = {"id": user_id, "title": title}
     users.append(user)
     return {"new user": user}
+
 
 
 @app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -31,6 +39,7 @@ async def delete_user(user_id: int):
         users.remove(user)
         return {"user is deleted": user}
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f" {user_id} not found")
+
 
 if __name__ == '__main__':
     uvicorn.run("__main__:app", reload=True)
