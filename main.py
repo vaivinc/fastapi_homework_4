5cf8fa5ad0834a41f15e8f85f20f19113e1be398
vaivinc <168576062+vaivinc@users.noreply.github.com>
2024-09-26 17:42:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,11 @@ async def all_users():
 @app.get('/users/{user_id}/')
 async def get_user_id(user_id: int):
     user = next((user for user in users if user["id"] == user_id), None)
-    if user:
-        return {"user": user}
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User with {user_id} not found")
+    return {"user": user}
 
 
-@app.post("/user/{user_id}/", status_code=status.HTTP_201_CREATED)
+
+@app.post("/users/{user_id}/", status_code=status.HTTP_201_CREATED)
 async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
                       title: str = Query(description="enter user")):
     user = {"id": user_id, "title": title}
@@ -32,7 +31,7 @@ async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
 
 
 
-@app.delete("/use/{user_id}/", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/users/{user_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int):
     user = next((user for user in users if user["id"] == user_id), None)
     if user:
@@ -43,3 +42,4 @@ async def delete_user(user_id: int):
 
 if __name__ == '__main__':
     uvicorn.run("__main__:app", reload=True)
+
