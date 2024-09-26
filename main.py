c6b226308159c4c08f9746c9ca4e1a19102255f2
vaivinc <168576062+vaivinc@users.noreply.github.com>
2024-09-26 16:02:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def all_users():
     return {"users": users}
 
 
-@app.get('/users/{user_id}')
+@app.get('/users/{user_id}/')
 async def get_user_id(user_id: int):
     user = next((user for user in users if user["id"] == user_id), None)
     if user:
@@ -23,7 +23,7 @@ async def get_user_id(user_id: int):
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"User with {user_id} not found")
 
 
-@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
+@app.post("/user/{user_id}/", status_code=status.HTTP_201_CREATED)
 async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
                       title: str = Query(description="enter user")):
     user = {"id": user_id, "title": title}
@@ -32,7 +32,7 @@ async def create_user(user_id: int = Path(gt=max([i["id"] for i in users])),
 
 
 
-@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/use/{user_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int):
     user = next((user for user in users if user["id"] == user_id), None)
     if user:
